60acf1bfd053d61da6fb070f5cf67b171a496591
acer <elmurodpro@gmail.com>
2025-05-14 22:59:20 +0500

    start bosilganda kampaniya haqida, filialalar, bo'sh ish o'rinlari menu yangiliklar, kontakt manzil, til degan butthonlar chiqadi va hammasi ishlaydi rasim, lacatsiya, nomer, va silka lar jo'natiladi va hammasi ishlayapti. eni databesini ulasa bo'ldi

--- databese.py
+++ databese.py
@@ -3,18 +3,22 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.conn = sqlite3.connect("evos.db")
-        self.cursor = self.conn.cursor()
+        self.connection = sqlite3.connect("evos.db")
+        self.cursor = self.connection.cursor()
+
     def create_user_table(self):
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS users (
-        id integer PRIMARY KEY,
-        first_name varchar(55) not null,
-        last_name varchar(55),
-        age integer,
-        address varchar(255),
-        telegram_id integer not null unique, 
-        lang varchar(55) default 'uz')
+        self.cursor.execute("""
+            create table if not exists users(
+            id integer primary key,
+            first_name varchar(55) not null,
+            last_name varchar(55),
+            age integer,
+            address varchar(255),
+            telegram_id integer not null unique,
+            lang varchar(55) default 'uz'
+            )
         """)
+
     def get_user(self, telegram_id):
         user = self.cursor.execute("""
         select * from users where telegram_id = ?
@@ -26,12 +30,19 @@ class Database:
         insert into users(first_name, last_name, telegram_id)
         values (?, ?, ?)
         """, (first_name, last_name, telegram_id))
-        self.conn.commit()
+        self.connection.commit()
 
     def get_user_lang(self, telegram_id):
         lang = self.cursor.execute("""
         select lang from users where telegram_id = ?
         """, (telegram_id,)).fetchone()
         return lang[0] if lang else "uz"
+    def set_user_lang(self, telegram_id, lang):
+        self.cursor.execute("""
+        update users set lang = ? where telegram_id = ?
+        """, (lang, telegram_id))
+        self.connection.commit()
+
+
 database = Database()
 database.create_user_table()
--- messages.py
+++ messages.py
@@ -1,10 +1,49 @@
 messages = {
     "uz": {
-        "about_company": "Kampaniya haqida",
-        "branches" : "Filialari",
+        "welcome_text": "Xush kelibsiz",
+        "about_company": "🏢 Kampaniya haqida",
+        "branches": "Filialari",
+        "job_positions": "Bo'sh ish o'rinlari",
+        "menu": "Menyu",
+        "news": "Yangiliklar",
+        "contacts": "Kantaktlar/Manzil",
+        "language": "🇺🇿/🇷🇺 Til",
+        "select_lang": "Tilni tanlang!",
+        "see_branches": "Filialarni ko'rish",
+        "head_office": "Bosh ofis",
+        "tashkent_city": "Toshkent shaxar"
     },
     "ru": {
-        "about_company": "О кампании",
-        "branches": "Филиалы"
+        "about_company": "🏢 О кампании",
+        "branches": "Филиалы",
+        "job_positions": "Вакансии",
+        "menu": "Меню",
+        "news": "Новости",
+        "contacts": "Контакты/Адрес",
+        "language": "🇺🇿/🇷🇺 Язык",
+        "select_lang": "Выберите язык!",
+        "welcome_text": "Добро пожаловать",
+
+    },
+    "en": {
+        "about_company": "About_company",
+        "branches": "Branches",
+        "job_positions": "Job_positions",
+        "menu": "Menu",
+        "news": "News",
+        "contacts": "Contacts",
+        "language": "Language",
+        "select_lang": "Select language",
+        "welcome_text": "Welcome text",
+
+    }
+}
+
+info_company = {
+    "uz": {
+        "company": ""
+    },
+    "ru": {
+        "company": ""
     }
 }
